30c042689245a656331513aa00665dbd4a6101b1
damajiang90 <w90ang@163.com>
2023-11-01 18:47:17 +0800

    添加appwidget的基础
    Signed-off-by: damajiang90 <w90ang@163.com>

---
+++ android/src/main/kotlin/tech/soit/quiet/appwidget/PlayerWidget.kt
@@ -0,0 +1,16 @@
+package tech.soit.quiet.appwidget
+
+import android.appwidget.AppWidgetProvider
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.SharedPreferences
+
+class PlayerWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+
+    }
+}
